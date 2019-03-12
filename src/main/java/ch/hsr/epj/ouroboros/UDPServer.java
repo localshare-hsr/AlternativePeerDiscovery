e43d5779a6903276f466120dbedb3f42a7692114
Pascal Knecht <pascal.knecht@hsr.ch>
2019-03-12 10:29:52 +0100

    fix ip address visibility in udp server output

--- src/main/java/ch/hsr/epj/ouroboros/UDPServer.java
+++ src/main/java/ch/hsr/epj/ouroboros/UDPServer.java
@@ -3,8 +3,10 @@ package ch.hsr.epj.ouroboros;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 public abstract class UDPServer implements Runnable {
 
@@ -23,25 +25,29 @@ public abstract class UDPServer implements Runnable {
   @Override
   public void run() {
     byte[] buffer = new byte[bufferSize];
-    try (DatagramSocket socket = new DatagramSocket(port)) {
-      socket.setSoTimeout(10000);
-      System.out.println(
-          "Listen on " + socket.getLocalAddress() + " port " + socket.getLocalPort());
-      while (true) {
-        DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
-        try {
-          socket.receive(incoming);
-          this.respond(socket, incoming);
-        } catch (SocketTimeoutException ignored) {
-        } catch (IOException ex) {
-          ex.printStackTrace();
+    try {
+      InetAddress myIP = InetAddress.getByName(DiscoveredIPList.getInstance().getIdentity());
+      try (DatagramSocket socket = new DatagramSocket(port, myIP)) {
+        socket.setSoTimeout(10000);
+        System.out.println(
+            "Listen on " + socket.getLocalAddress() + " port " + socket.getLocalPort());
+        while (true) {
+          DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+          try {
+            socket.receive(incoming);
+            this.respond(socket, incoming);
+          } catch (SocketTimeoutException ignored) {
+          } catch (IOException ex) {
+            ex.printStackTrace();
+          }
         }
+      } catch (SocketException e) {
+        e.printStackTrace();
       }
-    } catch (SocketException e) {
+    } catch (UnknownHostException e) {
       e.printStackTrace();
     }
   }
 
   public abstract void respond(DatagramSocket socket, DatagramPacket request) throws IOException;
-
 }
